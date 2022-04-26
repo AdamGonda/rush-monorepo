826e82d7a90677cf4bd56cdc9c7e09b90e2b75c9
adamgonda <adam.gonda@htecgroup.com>
2022-04-26 10:21:45 +0200

    add jest types to text

--- components/text/src/index.spec.tsx
+++ components/text/src/index.spec.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import Text from '.';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Text />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
